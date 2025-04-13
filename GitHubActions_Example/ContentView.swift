099d1e736cdc8b54a9421275251add5ecb62654c
Mahfuz <>
2025-04-13 00:18:23 -0400

    asf

---
+++ GitHubActions_Example/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  GitHubActions_Example
+//
+//  Created by Mahfuz on 2025-04-12.
+//
+
+import SwiftUI
+
+struct Item: Identifiable {
+    var id: UUID = UUID()
+    var title: String
+    var purdate: Date
+}
+
+var list: [Item] =
+    [
+        Item(title: "Hello", purdate: Date()),
+        Item(title: "World", purdate: Date()),
+        Item(title: "11111", purdate: Date())
+    ]
+
+ //asdf
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            ScrollView {
+                ForEach(list) { item in
+                    Text(item.title)
+                }
+            }
+            .background(Color.red)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHubActions_Example/GitHubActions_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubActions_ExampleApp.swift
+//  GitHubActions_Example
+//
+//  Created by Mahfuz on 2025-04-12.
+//
+
+import SwiftUI
+
+@main
+struct GitHubActions_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
